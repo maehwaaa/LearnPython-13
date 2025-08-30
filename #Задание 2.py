4deb2797343fbc0fe86da813c200142dfb5d6820
maehwa <68327208+maehwaaa@users.noreply.github.com>
2025-08-30 17:21:55 +0600

    Update #Задание 2.py

--- #Задание 2.py
+++ #Задание 2.py
@@ -5,28 +5,28 @@ class turtle:
         self.y = y  # текущая позиция Y
         self.s = s  # шаг (количество клеток за ход)
 
-    def go_up(self):
+    def go_up(self): # увеличивает y на s
         self.y += self.s
 
-    def go_down(self):
+    def go_down(self): # уменьшает y на s
         self.y -= self.s
 
-    def go_left(self):
+    def go_left(self): # уменьшает x на s
         self.x -= self.s
 
-    def go_right(self):
+    def go_right(self): # увеличивает y на s
         self.x += self.s
 
-    def evolve(self):
+    def evolve(self): # увеличивает s на 1
         self.s += 1
 
-    def degrade(self):
+    def degrade(self): # уменьшает s на 1 или выкидывает ошибку, когда s может стать ≤ 0
         if self.s > 1:
             self.s -= 1
         else:
             raise ValueError("Шаг не может быть ≤ 0!")
 
-    def count_moves(self, x2, y2):
+    def count_moves(self, x2, y2): # возвращает минимальное количество действий, за которое черепашка сможет добраться до x2 y2 от текущей позиции
         dx = abs(x2 - self.x)
         dy = abs(y2 - self.y)
 
